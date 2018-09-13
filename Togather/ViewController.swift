95439ddc4102da7305807d82cba0ac490cd0d5a3
juanmorillios <juanmorillios@gmail.com>
2018-09-13 23:20:58 +0200

    Initial Commit

---
+++ Togather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Togather
+//
+//  Created by Jmorillo on 13/9/18.
+//  Copyright © 2018 Fantastics Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
